61cff1c93108c1861065e3815ea8db5c145a06fb
Benedikt Beigang <benedikt.beigang@relaxdays.de>
2022-04-14 14:50:02 +0200

    add asp_net project

---
+++ dotnet-rpg/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos;
+using dotnet_rpg.Dtos.Character.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+
+            return (response.Success) ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(
+                request.Username, request.Password
+            );
+
+            return (response.Success) ? Ok(response) : BadRequest(response);
+        }
+    }
+}
---
+++ dotnet-rpg/Dtos/Skill/GetSkillDto.cs
@@ -0,0 +1,8 @@
+namespace dotnet_rpg.Dtos.Skill
+{
+    public class GetSkillDto
+    {
+        public string name { get; set; }
+        public int Damage { get; set; }
+    }
+}
